8419d9693841d4ec426fa2cff105352c09e8a0a5
Jan Dvořák <mordae@anilinux.org>
2013-05-15 23:23:21 +0200

    initial import
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

---
+++ dm/ffi.rkt
@@ -0,0 +1,152 @@
+#lang racket/base
+;
+; libdevmapper.so.1.02 bindings
+;
+
+(require racket/contract
+         (rename-in ffi/unsafe (-> -->))
+         ffi/unsafe/define)
+
+(provide (all-defined-out))
+
+
+(define-struct/contract (exn:fail:dm exn:fail) () #:transparent)
+
+
+(define-ffi-definer define-scheme #f)
+(define-ffi-definer define-dm (ffi-lib "libdevmapper" '("1.02")))
+
+
+(define/contract (check-result func result)
+                 (-> symbol? any/c void?)
+  (unless result
+    (raise (make-exn:fail:dm
+             (format "dm: ~a returned ~a\n" func result)
+             (current-continuation-marks)))))
+
+
+(define (with-finalizer result finalizer)
+  (when result
+    (register-finalizer result finalizer))
+  result)
+
+
+(define _string/utf-8/free
+  (make-ctype _bytes
+              (lambda (str)
+                (if str
+                  (string->bytes/utf-8 str)
+                  #f))
+              (lambda (bstr)
+                (if bstr
+                  (let ((str (bytes->string/utf-8 bstr)))
+                    (free bstr)
+                    str)
+                  #f))))
+
+
+(define-cpointer-type _dm-task-pointer)
+
+
+(define dm-task-types '(create reload remove remove-all suspend resume info
+                        deps rename version status table waitevent list clear
+                        mknodes list-versions target-msg set-geometry))
+
+(define _dm-task-type (_enum dm-task-types))
+
+(define (dm-task-type? v)
+  (and (member v dm-task-types) #t))
+
+
+(define-cstruct _struct-dm-info
+  ((exists         _bool)
+   (suspended      _bool)
+   (live-table     _bool)
+   (inactive-table _bool)
+   (open-count     _int32)
+   (event-nr       _uint32)
+   (major          _uint32)
+   (minor          _uint32)
+   (read-only      _bool)
+   (target-count   _int32)))
+
+
+(define-cstruct _dm-names
+  ((dev  _uint64)
+   (next _uint32)))
+
+
+(define-dm dm_task_destroy
+           (_fun _dm-task-pointer --> _void))
+
+(define-dm dm_task_create
+           (_fun _dm-task-type
+                 --> (result : _dm-task-pointer)
+                 --> (with-finalizer result dm_task_destroy)))
+
+(define-dm dm_task_set_name
+           (_fun _dm-task-pointer
+                 _string/utf-8
+                 --> (result : _bool)
+                 --> (check-result 'dm_task_set_name result)))
+
+(define-dm dm_task_set_uuid
+           (_fun _dm-task-pointer
+                 _string/utf-8
+                 --> (result : _bool)
+                 --> (check-result 'dm_task_set_uuid result)))
+
+(define-dm dm_task_set_newname
+           (_fun _dm-task-pointer
+                 _string/utf-8
+                 --> (result : _bool)
+                 --> (check-result 'dm_task_set_newname result)))
+
+(define-dm dm_task_set_newuuid
+           (_fun _dm-task-pointer
+                 _string/utf-8
+                 --> (result : _bool)
+                 --> (check-result 'dm_task_set_newuuid result)))
+
+(define-dm dm_task_run
+           (_fun _dm-task-pointer
+                 --> (result : _bool)
+                 --> (check-result 'dm_task_run result)))
+
+(define-dm dm_task_get_info
+           (_fun _dm-task-pointer
+                 (info : (_ptr o _struct-dm-info))
+                 --> (result : _bool)
+                 --> (begin
+                       (check-result 'dm_task_get_info result)
+                       info)))
+
+(define-dm dm_task_update_nodes
+           (_fun --> _void))
+
+(define-dm dm_task_add_target
+           (_fun _dm-task-pointer
+                 _uint64
+                 _uint64
+                 _string/utf-8
+                 _string/utf-8
+                 --> (result : _bool)
+                 --> (check-result 'dm_task_add_target result)))
+
+(define-dm dm_get_next_target
+           (_fun _dm-task-pointer
+                 _pointer
+                 (start : (_ptr o _uint64))
+                 (length : (_ptr o _uint64))
+                 (ttype : (_ptr o _string/utf-8/free))
+                 (params : (_ptr o _string/utf-8/free))
+                 --> (next : _pointer)
+                 --> (values start length ttype params next)))
+
+(define-dm dm_task_get_names
+           (_fun _dm-task-pointer
+                 --> (result : _dm-names-pointer)
+                 --> (with-finalizer result free)))
+
+
+; vim:set ts=2 sw=2 et:
---
+++ dm/main.rkt
@@ -0,0 +1,137 @@
+#lang racket/base
+;
+; Device Mapper Tools
+;
+
+(require (rename-in ffi/unsafe (-> -->))
+         (planet williams/uuid/uuid)
+         racket/contract
+         racket/string)
+
+(require "ffi.rkt")
+
+(define-struct/contract dm-info
+  ((exists         boolean?)
+   (suspended      boolean?)
+   (live-table     boolean?)
+   (inactive-table boolean?)
+   (open-count     integer?)
+   (event-nr       integer?)
+   (major          integer?)
+   (minor          integer?)
+   (read-only      boolean?)
+   (target-count   integer?))
+  #:transparent)
+
+
+(define-struct/contract dm-target
+  ((start  integer?)
+   (length integer?)
+   (type   symbol?)
+   (params (listof (or/c string? integer?))))
+  #:transparent)
+
+
+(provide (all-defined-out))
+
+
+(define/contract (dm-list)
+                 (-> (listof string?))
+  (let ((task (dm_task_create 'list)))
+    (dm_task_run task)
+    (let loop ((names (dm_task_get_names task)))
+      (let ((bstr (make-sized-byte-string (ptr-add names 12)
+                                          (- (dm-names-next names) 12))))
+        (let ((str (string-replace (bytes->string/utf-8 bstr) "\0" "")))
+          (if (> (dm-names-next names) 0)
+            (cons str (loop (ptr-add names (dm-names-next names))))
+            (list str)))))))
+
+
+(define/contract (dm-get-info name)
+                 (-> string? dm-info?)
+  (let ((task (dm_task_create 'info)))
+    (dm_task_set_name task name)
+    (dm_task_run task)
+    (apply dm-info (struct-dm-info->list (dm_task_get_info task)))))
+
+
+(define/contract (dm-get-table name)
+                 (-> string? (listof dm-target?))
+  (let ((task (dm_task_create 'table)))
+    (dm_task_set_name task name)
+    (dm_task_run task)
+    (let loop ((next #f))
+      (let-values (((start len ttype params next)
+                    (dm_get_next_target task next)))
+        (let ((value (dm-target start len (string->symbol ttype)
+                                          (map (lambda (p)
+                                                 (if (string->number p)
+                                                   (string->number p)
+                                                   p))
+                                                 (string-split params)))))
+          (if next
+            (cons value (loop next))
+            (list value)))))))
+
+
+(define/contract (dm-remove name)
+                 (-> string? void?)
+  (let ((task (dm_task_create 'remove)))
+    (dm_task_set_name task name)
+    (dm_task_run task)
+    (dm_task_update_nodes)))
+
+
+(define/contract (dm-rename name new-name #:new-uuid (new-uuid #f))
+                 (->* (string? string?) (#:new-uuid (or/c #f string?)) void?)
+  (let ((task (dm_task_create 'rename)))
+    (dm_task_set_name task name)
+    (dm_task_set_newname task new-name)
+    (when new-uuid
+      (dm_task_set_newuuid task new-uuid))
+    (dm_task_run task)
+    (dm_task_update_nodes)))
+
+
+(define/contract (dm-create name #:uuid (uuid #f) . targets)
+                 (->* (string?) (#:uuid (or/c #f string?))
+                      #:rest (listof dm-target?) void?)
+  (let ((task (dm_task_create 'create)))
+    (dm_task_set_name task name)
+    (dm_task_set_uuid task (if uuid uuid (uuid->string (make-uuid-4))))
+    (for-each (lambda (target)
+                (dm_task_add_target task
+                  (dm-target-start target)
+                  (dm-target-length target)
+                  (symbol->string (dm-target-type target))
+                  (string-join (map (lambda (p)
+                                      (if (number? p)
+                                        (number->string p)
+                                        p))
+                                    (dm-target-params target)) " ")))
+              targets)
+    (dm_task_run task)
+    (dm_task_update_nodes)))
+
+
+(define/contract (dm-reload name . targets)
+                 (->* (string?) () #:rest (listof dm-target?) void?)
+  (let ((task (dm_task_create 'reload)))
+    (dm_task_set_name task name)
+    (for-each (lambda (target)
+                (dm_task_add_target task
+                  (dm-target-start target)
+                  (dm-target-length target)
+                  (symbol->string (dm-target-type target))
+                  (string-join (map (lambda (p)
+                                      (if (number? p)
+                                        (number->string p)
+                                        p))
+                                    (dm-target-params target)) " ")))
+              targets)
+    (dm_task_run task)
+    (dm_task_update_nodes)))
+
+
+; vim:set ts=2 sw=2 et:
