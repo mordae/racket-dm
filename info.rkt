145423e41366182fc4fb5a74230c063bf116b95d
Jan Dvořák <mordae@anilinux.org>
2014-07-08 15:28:18 +0200

    added some missing deps
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("misc1" "planet-williams-uuid1" "planet-soegaard-digest1"))
+(define deps '("core" "scribble-lib" "misc1"
+               "planet-williams-uuid1" "planet-soegaard-digest1"))
 
 ; vim:set ts=2 sw=2 et:
