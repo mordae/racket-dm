5221b1fb7fceb981e450e4469d085e5513848358
Jan Dvořák <mordae@anilinux.org>
2014-07-10 14:10:11 +0200

    depend on "base" instead of "core"
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("core" "scribble-lib" "misc1"
+(define deps '("base" "scribble-lib" "misc1"
                "planet-williams-uuid1" "planet-soegaard-digest1"))
 
 ; vim:set ts=2 sw=2 et:
